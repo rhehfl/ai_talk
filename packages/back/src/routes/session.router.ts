7d17800afa6ca5c176b79caa3bcdbf247c9e7941
GuDoYoon <rhehfl418q@gmail.com>
2025-09-27 17:45:35 +0900

    Redis 클라이언트 주입: ChatRepository에 redisClient를 주입하여 세션 라우터에서 Redis를 사용할 수 있도록 수정

--- packages/back/src/routes/session.router.ts
+++ packages/back/src/routes/session.router.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { SessionController } from "../controllers/sessionController";
 import { SessionService } from "../services/sessionService";
 import { ChatRepository } from "../repositories/chatRepository";
-
+import { redisClient } from "../client";
 const router = Router();
 
-const chatRepository = new ChatRepository();
+const chatRepository = new ChatRepository(redisClient);
 const sessionService = new SessionService(chatRepository);
 const sessionController = new SessionController(sessionService);
 
