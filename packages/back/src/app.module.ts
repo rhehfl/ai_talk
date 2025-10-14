cba34c714c66c713e771c0aa0158f093d2726f5a
GuDoYoon <rhehfl418q@gmail.com>
2025-10-14 19:13:16 +0900

    fix: postgres연결 수정

--- packages/back/src/app.module.ts
+++ packages/back/src/app.module.ts
@@ -5,17 +5,22 @@ import { RedisModule } from './core/redis/redis.module';
 import { SessionModule } from './session/session.module';
 import { PersonasModule } from './personas/personas.module';
 import { ChatRoomsModule } from './chat_rooms/chat_rooms.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from 'typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `src/configs/env/.env.${process.env.NODE_ENV}`,
+      envFilePath: `./configs/env/.${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        typeORMConfig(configService),
+    }),
     RedisModule,
     SessionModule,
     PersonasModule,
--- packages/back/typeorm.config.ts
+++ packages/back/typeorm.config.ts
@@ -1,12 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME || 'ai_talk_db',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: process.env.NODE_ENV === 'dev',
+export const typeORMConfig: (
+  configService: ConfigService,
+) => TypeOrmModuleOptions = (configService) => {
+  console.log(configService.get('NODE_ENV') === 'dev');
+  return {
+    type: 'postgres',
+    host: configService.get('DATABASE_HOST'),
+    port: parseInt(configService.get('DATABASE_PORT') || '5432', 10),
+    username: configService.get('DATABASE_USER'),
+    password: configService.get<string>('DATABASE_PASSWORD') as string,
+    database: configService.get('DATABASE_NAME'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: configService.get('NODE_ENV') === 'dev',
+  };
 };
