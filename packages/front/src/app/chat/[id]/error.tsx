cbe9ce45e9a4d43e025ef7856bc56ce527a8c8b5
GuDoYoon <rhehfl418q@gmail.com>
2025-11-05 15:08:12 +0900

    fix: 오류 페이지 링크 클래스 수정

--- packages/front/src/app/chat/[id]/error.tsx
+++ packages/front/src/app/chat/[id]/error.tsx
@@ -27,7 +27,7 @@ export default function ChatErrorPage() {
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link
             href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400"
           >
             홈으로 돌아가기
           </Link>
